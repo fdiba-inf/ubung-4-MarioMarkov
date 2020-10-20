653e3be2b79a5d501935da6a4f16fabb489780d2
MarioMarkov <mario11septemvri@gmail.com>
2020-10-20 15:25:44 +0000

    2

---
+++ src/exercise4/MinIndex.java
@@ -0,0 +1,29 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinIndex {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int length = input.nextInt();
+        
+        double[] numbers = new double[length];
+        double maxnumber =0;
+        
+       for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = input.nextDouble();
+        }
+        for (int index = 1; index < numbers.length; index++) {
+            if(numbers[index]>numbers[index-1]){
+               maxnumber =numbers[index];
+            }
+        }
+
+            System.out.println("Max number: "+maxnumber);
+        
+    }
+
+}
